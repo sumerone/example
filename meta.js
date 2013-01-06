6b4da0b117ca89f348dcd9dd94738454699aa17f
songzhongkai <songzhongkai@gmail.com>
2013-01-06 12:08:31 +0800

    first

--- meta.js
+++ meta.js
@@ -1,14 +1,13 @@
 {
-    "site_name": "小天天博客",
-    "copyright": "wawasoft 2012",
+    "site_name": "宋仲凯的博客",
+    "copyright": "@sumerone 2012",
     "cates": [
         {"name": "tech", "text":"技术"},
-        {"name": "art", "text":"艺术"},
+        {"name": "life", "text":"生活"},
+	{"name": "work", "text":"工作"},
         {"name": "other", "text":"其它"}
     ],
     "articles":[
-        {"title": "tech hello world", "file": "2012-01-23", "cate":"tech"},
-        {"title": "art hello world", "file": "2012-02-23", "cate":"art"},
-        {"title": "other hello world", "file": "2012-03-23", "cate":"other"}
+        {"title": "2012总结", "file": "2013-01-06", "cate":"other"}
         ]
 }
